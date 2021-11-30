b928d11bd51c0b9fcc43ced9904c7ca8ac636951
Yossi Farjoun <farjoun@gmail.com>
2021-11-30 11:30:51 -0500

    adding reference files

--- installation/03-post-conda-step.sh
+++ installation/03-post-conda-step.sh
@@ -30,3 +30,17 @@ remotes::install_github("davidaknowles/leafcutter/leafcutter", ref = "psi_2019")
 EOF
 
 git clone https://github.com/RajLabMSSM/leafcutter-pipeline.git
+
+cp example.yaml leafcutter-pipeline/examples/
+
+mkdir reference
+pushd reference
+
+curl -L https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_38/gencode.v38.annotation.gtf.gz --output gencode.v38.annotation.gtf.gz
+tabix index gencode.v38.annotation.gtf.gz
+
+curl -L https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_38/GRCh38.p13.genome.fa.gz --output GRCh38.p13.genome.fa.gz
+bwa index GRCh38.p13.genome.fa.gzGRCh38.p13.genome.fa.gz
+
+ls
+popd 
