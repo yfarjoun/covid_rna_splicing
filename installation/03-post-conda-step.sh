f0c292638bee5982c84831d8cd1e674f9bdd34ae
Yossi Farjoun <farjoun@gmail.com>
2021-12-02 14:59:17 -0500

    not bgzf

--- installation/03-post-conda-step.sh
+++ installation/03-post-conda-step.sh
@@ -29,7 +29,7 @@ mkdir reference
 pushd reference
 
 curl -L https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_38/gencode.v38.annotation.gtf.gz --output gencode.v38.annotation.gtf.gz
-tabix gencode.v38.annotation.gtf.gz
+# tabix gencode.v38.annotation.gtf.gz
 
 curl -L https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_38/GRCh38.p13.genome.fa.gz --output GRCh38.p13.genome.fa.gz
 bwa index GRCh38.p13.genome.fa.gzGRCh38.p13.genome.fa.gz
