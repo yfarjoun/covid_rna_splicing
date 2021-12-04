33dda4746b7ccedd9d8acf507362eb198410d8bb
Yossi Farjoun <farjoun@gmail.com>
2021-12-04 13:42:22 -0500

    - linting some bash - removing duplicate download of reference - removing commented code

--- installation/04-post-conda-step.sh
+++ installation/04-post-conda-step.sh
@@ -27,17 +27,6 @@ git clone https://github.com/RajLabMSSM/leafcutter-pipeline.git
 
 git clone https://github.com/davidaknowles/leafcutter.git
 
-mkdir reference
-pushd reference
-
-curl -L https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_38/gencode.v38.annotation.gtf.gz --output gencode.v38.annotation.gtf.gz
-# tabix gencode.v38.annotation.gtf.gz
-
-curl -L https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_38/GRCh38.p13.genome.fa.gz --output GRCh38.p13.genome.fa.gz
-bwa index GRCh38.p13.genome.fa.gz
-
-ls
-popd 
 
 export TAR=/bin/tar
 
--- scripts/reheader_corrupt_bams.sh
+++ scripts/reheader_corrupt_bams.sh
@@ -2,16 +2,15 @@
 
 set -xeuo pipefail
 
-input=$1
+input="$1"
 
-# sed -n '1,1{s/^.*@HD/@HD/p}; 2,2{s/^/@RG\t/p}; 3,/@CO/{p}'  <(zless ${input}) > header.sam
-sed -n '1,1 s/^.*@HD/@HD/p; 2,2 s/^/@RG\t/p; 3,/@CO/ p'  <(zless ${input}) > header.sam
+sed -n '1,1 s/^.*@HD/@HD/p; 2,2 s/^/@RG\t/p; 3,/@CO/ p'  <(zless "${input}") > header.sam
 
 cat << "EOF" >> header.sam
 @CO	manual fixing of header using:  "sed -n '1,1 s/^.*@HD/@HD/p; 2,2 s/^/@RG\t/p; 3,/@CO/ p' "
 EOF
 
-samtools reheader header.sam $input > temp.bam
+samtools reheader header.sam "$input" > temp.bam
 
 nlines=$(samtools view temp.bam | wc -l)
 
@@ -20,5 +19,5 @@ if [[ $nlines -eq 0 ]]; then
 else
 	echo "resulting bam file has $nlines lines"
 fi
-mv temp.bam $input
+mv temp.bam "$input"
 	
